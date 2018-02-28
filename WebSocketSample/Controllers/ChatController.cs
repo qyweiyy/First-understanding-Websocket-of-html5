ec8dc6b91af0c39cb60b79f9e40e8fcca8867f13
qyweiyy <qyweiyy@163.com>
2018-02-28 18:05:46 +0800

    Websocket Demo
    Readed massive information for about websocket of html5,I find this demo. Run this demo,html address is 'http://localhost:http://localhost:55945/WebSocket/Index'. You mast edit address's nickName to send message for websocket- ws = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port + '/api/chat?nickName=test').

---
+++ WebSocketSample/Controllers/ChatController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using BCL.WebSockets;
+
+namespace WebSocketSample.Controllers
+{
+    [RoutePrefix("api/chat")]
+    public class ChatController : ApiController
+    {
+        private static Dictionary<string, WebSocketHandler> _handlers = new Dictionary<string, WebSocketHandler>();
+
+        [Route]
+        [HttpGet]
+        public async Task<HttpResponseMessage> Connect(string nickName)
+        {
+            if (string.IsNullOrEmpty(nickName))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var webSocketHandler = new WebSocketHandler();
+            if (_handlers.ContainsKey(nickName))
+            {
+                var origHandler = _handlers[nickName];
+                await origHandler.Close();
+            }
+
+            _handlers[nickName] = webSocketHandler;
+
+            webSocketHandler.TextMessageReceived += ((sendor, msg) =>
+            {
+                BroadcastMessage(nickName, nickName + " Says: " + msg);
+                _handlers[nickName].SendMessage("你好！"+nickName+",你的信息是："+msg);
+                //foreach(var item in _handlers.Keys)
+                //{
+                //    if (item.Equals(nickName))
+                //        continue;
+                //    _handlers[item].SendMessage(nickName + "发的信息是：" + msg);
+                //}
+            });
+
+            webSocketHandler.Closed += (sendor, arg) =>
+            {
+                BroadcastMessage(nickName, nickName + " Disconnected!");
+                _handlers.Remove(nickName);
+            };
+
+            webSocketHandler.Opened += (sendor, arg) =>
+                        {
+                            BroadcastMessage(nickName, nickName + " Connected!");
+                        };
+
+            HttpContext.Current.AcceptWebSocketRequest(webSocketHandler);
+
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+
+        private void BroadcastMessage(string sendorNickName, string message)
+        {
+            foreach (var handlerKvp in ChatController._handlers)
+            {
+                if (handlerKvp.Key != sendorNickName)
+                {
+                    handlerKvp.Value.SendMessage(message).Wait();
+                }
+            }
+        }
+    }
+}
